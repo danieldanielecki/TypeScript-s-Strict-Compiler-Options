426913710ffc9297eb9c2264cd68eb41882d8167
Daniel Danielecki <daniel.danielecki@mail.com>
2021-10-04 13:04:36 +0200

    Add: useUnknownInCatchVariables example

---
+++ examples/08-useUnknownInCatchVariables/08-useUnknownInCatchVariables.ts
@@ -0,0 +1,12 @@
+function main() {
+  console.log("main");
+  try {
+    console.log("try");
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(err.message);
+    }
+  }
+}
+
+main();
